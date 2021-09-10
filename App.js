ba20769881a2d5e3e577ad4f51046335a548a6b0
AnchiTeng <84085487+AnchiTeng@users.noreply.github.com>
2021-09-10 10:15:14 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
-//import logo from './logo.svg';
 import './App.css';
-//import MemberSideBar from './components/homePage/homePage';
+import { Router, Route, Switch,browserHistory } from "react-router";
+import MemberSideBar from './components/homePage/homePage';
 //import VideoSlide from './components/videoSlide/videoSlide';
 import MyVideo from './components/userPage/myVideo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    // <div className="App">
+    //   <header className="App-header">
         
-        <h4>solo project</h4> 
+    //     <h4>solo project</h4> 
        
-      </header>
+    //   </header>
       
       
-      <MyVideo/>
-    </div>
+    //   <MyVideo/>
+    // </div>
+    <main>
+    <Switch>
+      <Route exact path='/' component={MemberSideBar} />
+      <Route exact path='/myvideo' component={MyVideo} />
+      
+    </Switch>
+  </main>
   );
 }
-
+//test
 export default App;
