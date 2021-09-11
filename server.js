7d41d961999f8b6ea439c1f4827c24a6542896c8
Anchi Teng <anchi1009@gmail.com>
2021-09-12 04:53:03 +0800

    9/11

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ app.get('/myvideo',(req,res)=>{
 })
 
 app.post('/myvideo',(req,res)=>{
+  console.log('inside the myvideo endpoint');
   if(req.files === null){
     return res.status(400).json({msg:'no file upload'});
   }
   const file = req.files.file;
-  console.log('list',req.body)
+  
  
 
   
@@ -49,17 +50,17 @@ app.post('/myvideo',(req,res)=>{
 
 
 app.get('/api/customer', (req,res)=>{
-    const customers = [
+    const customer= [
       {id: 1, firstName: 'Angel', lastName:'Teng'},
       {id: 2, firstName: 'Finn', lastName:'Lo'}, 
       {id: 3, firstName: 'Sam', lastName:'Lo'}   
     ];
 
-    res.json(customers);
+    res.json(customer);
 })
 
 
 
 const port = 5000;
 
-app.listen(port, () => console.log('Server started on port ${port}'));
+app.listen(port, () => console.log(`Server started on port ${port}`));
