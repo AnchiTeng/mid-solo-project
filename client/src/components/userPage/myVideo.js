edcb4c5dfe609e7f745d4ed0762b0c0bbaaf29d8
tommyrharper <thomasrobertharper@gmail.com>
2021-09-11 23:01:41 +0100

    Add client and remove client2

---
+++ client/src/components/userPage/myVideo.js
@@ -0,0 +1,89 @@
+import React, { Fragment, useState } from "react";
+import axios from "axios";
+
+const MyVideo = () => {
+  const [file, setFile] = useState("");
+  const [filename, setFilename] = useState("abcd");
+  const [uploadedFile, setUploadedFile] = useState({});
+  //const [fileList,setFileList] = useState([]);
+
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+    //setFileList(e.target.files[0].fileList);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file); //'file' refers to  server.js 'post' const file = req.files.file;
+    try {
+      const res = await axios.post("/myvideo", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setFilename("coconuts");
+
+      const { fileName, filePath, uploadDate } = res.data;
+      const file = { fileName, filePath, uploadDate };
+      setUploadedFile(file);
+      // setFileList.push(fileName, filePath ,uploadDate)
+    } catch (err) {
+      if (err.response.status === 500) {
+        //setMessage('There was a problem with the server');
+      } else {
+        //setMessage(err.response.data.msg);
+      }
+      //setUploadPercentage(0)
+    }
+  };
+
+  return (
+    <Fragment>
+      <div className="custom-file mb-4">
+        <input
+          type="file"
+          className="custom-file-input"
+          id="customFile"
+          onChange={onChange}
+        />
+        <label className="custom-file-label" htmlFor="customFile">
+          {filename}
+        </label>
+        <button onClick={onSubmit}>Upload</button>
+      </div>
+      {uploadedFile ? (
+        <div className="row mt-5">
+          <div className="col-md-6 m-auto">
+            <h3 className="text-center">
+              {uploadedFile.uploadDate} {uploadedFile.fileName}{" "}
+            </h3>
+
+            <video
+              key={uploadedFile.filePath}
+              width="320"
+              height="240"
+              controls
+            >
+              <source src={uploadedFile.filePath} type="video/mp4" />
+            </video>
+
+            <h2>
+              video list: {uploadedFile.fileName}
+              <input
+                type="submit"
+                value="delete"
+                className="btn btn-primary btn-block mt-4"
+              />
+            </h2>
+          </div>
+        </div>
+      ) : null}
+    </Fragment>
+  );
+};
+
+//<img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
+
+export default MyVideo;
--- server.js
+++ server.js
@@ -1,65 +1,50 @@
-const express = require('express');
-const fileUpLoad = require('express-fileupload');
+const express = require("express");
+const fileUpLoad = require("express-fileupload");
 
 const app = express();
 
 app.use(fileUpLoad());
 
+app.get("/myvideo", (req, res) => {
+  const test = [{ test: 1 }];
 
-app.get('/myvideo',(req,res)=>{
-  
- const test = [{test:1}]
- 
   res.send(test);
-  
-})
+});
 
-app.post('/myvideo',(req,res)=>{
-  console.log('inside the myvideo endpoint');
-  if(req.files === null){
-    return res.status(400).json({msg:'no file upload'});
+app.post("/myvideo", (req, res) => {
+  console.log("inside the myvideo endpoint");
+  if (req.files === null) {
+    return res.status(400).json({ msg: "no file upload" });
   }
   const file = req.files.file;
-  console.log('list',req.body)
- 
-
-  
+  console.log("list", req.body);
 
   //sending uploadfiles to uploads folder
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, (err) => {
     if (err) {
       console.error(err);
       return res.status(500).send(err);
     }
-    
-    let today = new Date().toISOString().slice(0, 10)
+
+    let today = new Date().toISOString().slice(0, 10);
     //filePath is from uploads folder
-   res.json({fileName:file.name, filePath: `/uploads/${file.name}`,uploadDate:today});
-   
+    res.json({
+      fileName: file.name,
+      filePath: `/uploads/${file.name}`,
+      uploadDate: today,
+    });
+  });
+});
 
-  
+app.get("/api/customer", (req, res) => {
+  const customer = [
+    { id: 1, firstName: "Angel", lastName: "Teng" },
+    { id: 2, firstName: "Finn", lastName: "Lo" },
+    { id: 3, firstName: "Sam", lastName: "Lo" },
+  ];
 
-   
-   
-  })
-  
-})
-
-
-
-
-
-app.get('/api/customer', (req,res)=>{
-    const customer= [
-      {id: 1, firstName: 'Angel', lastName:'Teng'},
-      {id: 2, firstName: 'Finn', lastName:'Lo'}, 
-      {id: 3, firstName: 'Sam', lastName:'Lo'}   
-    ];
-
-    res.json(customer);
-})
-
-
+  res.json(customer);
+});
 
 const port = 5000;
 
